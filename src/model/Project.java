9d068305537d929301e2294bf03965a19284e30a
Daria Zelenova <muzzy404@gmail.com>
2018-05-27 16:15:37 +0300

    some actions and ProjectDAO done

--- src/model/Project.java
+++ src/model/Project.java
@@ -5,6 +5,14 @@ import javafx.beans.property.*;
 import java.sql.Date;
 
 public class Project {
+    protected static String ID = "ID";
+    protected static String NAME = "NAME";
+    protected static String COST = "COST";
+    protected static String DEPARTMENT_ID = "DEPARTMENT_ID";
+    protected static String DATE_BEG = "DATE_BEG";
+    protected static String DATE_END = "DATE_END";
+    protected static String DATE_END_REAL = "DATE_END_REAL";
+
     private IntegerProperty id = new SimpleIntegerProperty();
     private StringProperty name = new SimpleStringProperty();
     private DoubleProperty cost = new SimpleDoubleProperty();
@@ -14,6 +22,17 @@ public class Project {
     private SimpleObjectProperty<Date> date_end_real = new SimpleObjectProperty<>();
 
     public Project() {}
+
+    public Project(int id, String name, double cost, int dep_id,
+                   Date date_beg, Date date_end, Date date_end_real) {
+        this.id.set(id);
+        this.name.set(name);
+        this.cost.set(cost);
+        this.department_id.set(dep_id);
+        this.date_beg.set(date_beg);
+        this.date_end.set(date_end);
+        this.date_end_real.set(date_end_real);
+    }
 
     public void setId(int id) { this.id.set(id); }
     public int  getId()       { return id.get(); }
--- src/model/ProjectDAO.java
+++ src/model/ProjectDAO.java
@@ -1,4 +1,35 @@
 package model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import util.DBUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ProjectDAO {
+    public static ObservableList<Project> selectAll() throws SQLException {
+        String select = "SELECT * FROM BOOKKEEPING.PROJECTS";
+
+        ResultSet resultSet = DBUtil.dbExecuteSelect(select);
+        return getProjectsList(resultSet);
+    }
+
+    private static ObservableList<Project> getProjectsList(ResultSet set) throws SQLException {
+        ObservableList<Project> list = FXCollections.observableArrayList();
+
+        while(set.next()) {
+            Project project = new Project(
+                    set.getInt(Project.ID),
+                    set.getString(Project.NAME),
+                    set.getDouble(Project.COST),
+                    set.getInt(Project.DEPARTMENT_ID),
+                    set.getDate(Project.DATE_BEG),
+                    set.getDate(Project.DATE_END),
+                    set.getDate(Project.DATE_END_REAL));
+            list.add(project);
+        }
+
+        return list;
+    }
 }
