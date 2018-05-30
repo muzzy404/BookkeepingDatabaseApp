d5b1714d4c1870c30fd2db63ba5510d6e77fee85
Daria Zelenova <muzzy404@gmail.com>
2018-05-30 03:41:21 +0300

    selection of departments

--- src/controller/DepartmentsController.java
+++ src/controller/DepartmentsController.java
@@ -1,14 +1,19 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import model.Department;
 import model.DepartmentDAO;
 
 import java.sql.SQLException;
 
 public class DepartmentsController {
+    private static final int NO_ID = -1;
+    private int selectedDepartmentId = NO_ID;
 
     @FXML
     private TableView departmentTable;
@@ -18,6 +23,11 @@ public class DepartmentsController {
     @FXML
     private TableColumn<Department, String> columnName;
 
+    @FXML
+    private Label labelSelectedDepartment;
+    @FXML
+    private TextField fieldDepartmentName;
+
 
     private void showAllDepartments() {
         try {
@@ -26,6 +36,7 @@ public class DepartmentsController {
             System.out.println("showAllDepartments ERROR: " + e.getSQLState());
             e.printStackTrace();
         }
+        clearAllSelected();
     }
 
     @FXML
@@ -35,5 +46,22 @@ public class DepartmentsController {
 
         columnId.setVisible(false);
         showAllDepartments();
+    }
+
+    public void selectDepartment(MouseEvent mouseEvent) {
+        Department selected = (Department) departmentTable.getSelectionModel().getSelectedItem();
+        if (selected == null) return;
+
+        selectedDepartmentId = selected.getId();
+
+        labelSelectedDepartment.setText(selected.getName());
+        fieldDepartmentName.setText(selected.getName());
+    }
+
+    private void clearAllSelected() {
+        labelSelectedDepartment.setText("");
+        fieldDepartmentName.clear();
+
+        selectedDepartmentId = NO_ID;
     }
 }
